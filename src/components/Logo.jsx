04a519f004d9ca44d8d4271bc4b3c7bccead8bad
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-09-03 08:26:31 +0100

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { motion } from "framer-motion";
-import Image from "next/image";
+ Image from "next/image";
 import logo from "public/images/logo.jpg";
 
 const Logo = () => {
